1df2c009b66c1d22f67585eade608cf2eefa2929
Alex Kimeu <alexkimeu1999@gmail.com>
2024-04-17 13:06:28 +0300

    Initialised repo

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      boxShadow: {
+        'md': '0 0 10px rgba(0, 0, 0, 0.1)',
+      },
+      colors: {
+        'primary-light': '#FFFFFF',
+        'primary-dark': '#3F4040',
+        'secondary': '#48937e',
+        'variant1-light': '#F3F4F6',
+        'variant1-dark': '#323434',
+        'Heading': '#4B5563',
+        'red': '#FF3C5F'
+      }
+    },
+  },
+  plugins: [],
+}
