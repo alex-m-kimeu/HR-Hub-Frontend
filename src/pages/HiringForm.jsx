1715ed921e2cfaef86e0315a9fcbb3aae1628e34
Hamdi Adan <hhhaji013@gmail.com>
2024-04-18 00:59:33 +0300

    HiringPage Form

--- src/pages/HiringForm.jsx
+++ src/pages/HiringForm.jsx
@@ -1,11 +1,11 @@
+// src/components/HiringPage.jsx
+
 import React from 'react';
 
-const HiringPage = () => {
+export const HiringPage = () => {
   return (
     <div>
       <h1>Hiring Page</h1>
     </div>
   );
-};
-
-export default HiringPage;
+}
