799c5ab0dfcb8b986791caa45f2a23abf2ea24ab
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-04-18 06:11:10 +0300

    added sidebar for admin and employee

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { SidebarAdmin } from "./Components/SideBar/SidebarAdmin";
+import { SidebarEmployee } from "./Components/SideBar/SidebarEmployee";
+
 function App() {
   return (
     <>
+    <SidebarEmployee/>
     </>
   );
 }
