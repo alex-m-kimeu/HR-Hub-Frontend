16c5401942beb0fc3ab900f8841cf2d91d92b9ef
Anna Kioko <annakioko00@gmail.com>
2024-04-18 10:08:28 +0300

    table structure

--- src/pages/Leave/LeaveAdmin.jsx
+++ src/pages/Leave/LeaveAdmin.jsx
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
+
+
 export const LeaveAdmin = () => {
+
     return (
+      <div>
         <div>
-            <h1>Leave Admin</h1>
+          <h1>Leave</h1>
+          <button>Create time off</button>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Employee</th>
+                  <th>Start date</th>
+                  <th>End Date</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
         </div>
-    )
+      </div>
+    );
 };
--- src/pages/Leave/LeaveEmployee.jsx
+++ src/pages/Leave/LeaveEmployee.jsx
@@ -1,7 +1,36 @@
+import React, { useState } from "react";
+
 export const LeaveEmployee = () => {
-    return (
-        <div>
-            <h1>Leave Employee</h1>
-        </div>
-    )
+   return (
+     <div>
+       <div>
+         <h1 className="text lg font-extrabold">My Applications</h1>
+         <button type="">Create time off</button>
+         <div>
+           <table>
+             <thead>
+               <tr className="">
+                 <th>ID</th>
+                 <th>Employee</th>
+                 <th>Start date</th>
+                 <th>End Date</th>
+                 <th>Status</th>
+               </tr>
+             </thead>
+           </table>
+         </div>
+         <div>
+           <h1>leave Application</h1>
+           <form>
+             <input id="leave Type"type="text" placeholder="Leave Type" />
+             <input id="Start Date" type="text" placeholder="Start date" />
+             <input id ="End Date"type="text" placeholder="End date" />
+             <button type="submit">Submit</button>
+                   </form>
+                   <h1>Leave balance</h1>
+
+         </div>
+       </div>
+     </div>
+   );
 };
