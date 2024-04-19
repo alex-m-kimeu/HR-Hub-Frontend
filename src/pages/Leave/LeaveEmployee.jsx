8ee1bf5c91611466523dae221a00551cbc76b3c6
Alex Kimeu <alexkimeu1999@gmail.com>
2024-04-19 06:21:06 +0300

    Restructures how reviews were being displayed on admin side

--- src/pages/Leave/LeaveEmployee.jsx
+++ src/pages/Leave/LeaveEmployee.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+
 import { jwtDecode } from "jwt-decode";
 
 export const LeaveEmployee = () => {
